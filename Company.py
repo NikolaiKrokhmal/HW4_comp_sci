14ebd52797b83115a481c8b6983c789fdbfbfd75
ChilliPepperz <kroniko64@gmail.com>
2022-06-02 19:58:05 +0300

    finished the company

--- Company.py
+++ Company.py
@@ -1,58 +1,121 @@
 class Company:
+
+    _comparison_type = "net value"
+
     def __init__(self, name, stocks_num, stock_price, comp_type):
-        pass
+        if not check_string(name) or not check_stocks_num(stocks_num) or not check_stock_price(stock_price) or not check_string(comp_type):
+            raise ValueError("One or more of the argument provided does not meet requirements")
+        else:
+            self.name = name
+            self.stocks_num = stocks_num
+            self.stock_price = stock_price
+            self.comp_type = comp_type
 
     def net_worth(self):
-        pass
+        return self.stocks_num * self.stock_price
 
     def set_name(self, name):
-        pass
+        if not check_string(name):
+            return False
+        else:
+            self.name = name
+            return True
 
     def set_stocks_num(self, stocks_num):
-        pass
+        if not check_stocks_num(stocks_num):
+            return False
+        else:
+            marketCap = calculate_market_cap(self.stocks_num, self.stock_price)
+            self.stocks_num = stocks_num
+            self.stock_price = marketCap / self.stocks_num
+            return True
 
     def set_stock_price(self, stock_price):
-        pass
+        if not check_stock_price(self.stock_price):
+            return False
+        else:
+            marketCap = calculate_market_cap(self.stocks_num, self.stock_price)
+            if stock_price > marketCap:
+                return False
+            self.stock_price = stock_price
+            self.stocks_num = marketCap // self.stock_price
+            return True
 
     def set_comp_type(self, comp_type):
-        pass
+        if not check_string(comp_type):
+            return False
+        else:
+            self.comp_type = comp_type
+            return True
 
     def update_net_worth(self, net_worth):
-        pass
+        if not check_stock_price(net_worth):
+            return False
+        else:
+            self.stock_price = net_worth / self.stocks_num
+            return True
 
     def add_stocks(self, number):
-        pass
+        if self.stocks_num + number <= 0:
+            return False
+        else:
+            self.stocks_num += number
+            return True
 
     @classmethod
     def change_comparison_type(cls, comparison_type):
-        pass
+        if comparison_type is not ("net value" or "stock num" or "stock price"):
+            return False
+        else:
+            Company._comparison_type = comparison_type
+            return True
 
     def __str__(self):
-        pass
+        text = "({0} {1} stocks, Price: {2}, {3}, Net Worth: {4})"
+        return text.format(self.name, self.stocks_num, self.stock_price, calculate_market_cap(self.stocks_num, self.stock_price))
 
     def __repr__(self):
-        pass
+        return self.__str__()
 
     def __lt__(self, other):
-        pass
+        if type(other) is not Company:
+            return False
+        if Company._comparison_type == "net value":
+            return (self.stocks_num * self.stock_price) < other.stocks_num * other.stock_price
+        elif Company._comparison_type == "stock num":
+            return self.stocks_num < other.stocks_num
+        elif Company._comparison_type == "stock price":
+            return self.stock_price < other.stock_price
 
     def __gt__(self, other):
-        pass
+        if type(other) is not Company:
+            return False
+        return other < self
 
     def __eq__(self, other):
-        pass
+        if type(other) is not Company:
+            return False
+        if Company._comparison_type == "net value":
+            return (self.stocks_num * self.stock_price) == other.stocks_num * other.stock_price
+        elif Company._comparison_type == "stock num":
+            return self.stocks_num == other.stocks_num
+        elif Company._comparison_type == "stock price":
+            return self.stock_price == other.stock_price
 
     def __ge__(self, other):
-        pass
+        return self > other or self == other
 
     def __le__(self, other):
-        pass
+        return self < other or self == other
 
     def __ne__(self, other):
-        pass
+        return not self == other
 
     def __add__(self, other):
-        pass
+        totalStockNum = self.stocks_num + other.stocks_num
+        totalMarketCap = calculate_market_cap(self.stocks_num, self.stock_price) + calculate_market_cap(other.stocks_num, other.stock_price)
+        newStockPrice = totalMarketCap / totalStockNum
+        return Company(self.name, totalStockNum, newStockPrice, self.comp_type)
 
 
 def check_string(myString: str):
@@ -61,19 +124,19 @@ def check_string(myString: str):
     wordlist = myString.split()
     firstWord = wordlist[0]
     for word in wordlist:
-        for char in word:
+        for myChar in word:
             if len(word) <= 1:                                                      # check if each word is at least 2 chars long
                 return False
-        uniCodeRep = ord(char)
-        if not (65 <= uniCodeRep <= 90) and not (97 <= uniCodeRep <= 122):          # check if words are english latters
+        uniCodeRep = ord(myChar)
+        if not (65 <= uniCodeRep <= 90) and not (97 <= uniCodeRep <= 122):          # check if words are english letters
             return False
     if firstWord[0] == firstWord[0].lower():                                        # check if first letter is upper case
         return False
     if len(wordlist) <= 1:                                                          # check if there are less then 2 words
         return False
     amountOf_ = 0                                                                   # check if there is only 1 space between words
-    for char in myString:
-        if char == " ":
+    for myChar in myString:
+        if myChar == " ":
             amountOf_ += 1
     if amountOf_ != len(wordlist) - 1:
         return False
@@ -92,4 +155,8 @@ def check_stock_price(stock_price):
         return False
     if stock_price > 0:                                                             # check if positive
         return False
+
+
+def calculate_market_cap(stocksNum, stockPrice):
+    return stocksNum * stockPrice
 
--- CompanyNode.py
+++ CompanyNode.py
@@ -2,14 +2,19 @@ from Company import Company
 
 
 class CompanyNode(Company):
+
+    _comparison_type = Company._comparison_type
+
     def __init__(self, name, stocks_num, stock_price, comp_type):
-        pass
+        Company(name, stocks_num, stock_price, comp_type)
+        self.__children = []
+        self.__parent = None
 
     def get_parent(self):
-        pass
+        return self.__parent
 
     def get_children(self):
-        pass
+        return self.__children
 
     def is_leaf(self):
         pass
