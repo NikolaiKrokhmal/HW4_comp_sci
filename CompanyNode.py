d2f1847d887ba8c44a20bddd580858626de00d62
ChilliPepperz <kroniko64@gmail.com>
2022-06-03 19:19:11 +0300

    added a bunch of shit half way through the Company Node class

--- CompanyNode.py
+++ CompanyNode.py
@@ -22,14 +22,25 @@ class CompanyNode(Company):
         else:
             return False
 
-    def add_child(self, child):
-        pass
+    def add_child(self, child: "CompanyNode"):
+        if self.check_rule(child):
+            self.__children.append(child)
+            child.__parent = self.name
+            return True
+        else:
+            return False
 
     def total_net_worth(self):
-        pass
+        return self.get_total_sum()
 
     def test_node_order_validity(self):
-        pass
+        for child in self.get_children():
+            if not self.check_rule(child):
+                return False
+        while self.get_parent() is not None:
+            if not self.get_parent().check_rule(self):
+                return False
+        return True
 
     def __len__(self):
         return len(self.__children)
@@ -37,20 +48,25 @@ class CompanyNode(Company):
     def __repr__(self):
         pass
 
-    def is_ancestor(self, other):
+    def is_ancestor(self, other: "CompanyNode"):
+        while other.get_parent() is not None:
+            if self.name == other.get_parent():
+                return True
+            else:
+                other = other.get_parent()
+        return False
+
+    def __add__(self, other: "CompanyNode"):
         pass
 
-    def __add__(self, other):
-        pass
-
-    def check_rule(self, other):
+    def check_rule(self, other: "CompanyNode"):
         if self._comparison_type == "net value":
             if self.get_market_cap() >= other.get_market_cap():
                 return True
             else:
                 return False
         elif self._comparison_type == "stock num":
-            if self.stocks_num >= other.stock_num:
+            if self.stocks_num >= other.stocks_num:
                 return True
             else:
                 return False
@@ -60,4 +76,16 @@ class CompanyNode(Company):
             else:
                 return False
         elif self._comparison_type == "total sum":
-            pass
+            if self.get_total_sum() > other.get_total_sum():
+                return True
+            else:
+                return False
+
+    def get_total_sum(self):
+        totalValue = self.get_market_cap()
+        child_value = 0
+        if len(self.__children) == 0:
+            return totalValue
+        for child in self.__children:
+            child_value += child.get_total_sum()
+        return totalValue + child_value
